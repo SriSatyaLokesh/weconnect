5c97ee9d1207a1a65021b2ea43bb445b01f64af0
Satya <43106076+SriSatyaLokesh@users.noreply.github.com>
2020-03-12 22:38:02 +0530

    abusive words

--- src/tweets/validators.py
+++ src/tweets/validators.py
@@ -5,4 +5,13 @@ def validate_content(value):
     content = value
     if content == "":
         raise ValidationError("Content cannot be blank")
-    return value
+    abusive_words = ["fuck","fcuk","asshole","ass","poke","prick","bastard","arse","butt","porn","fecker","bitch","shit"]
+    valid = True
+    content = content.lower()
+    for word in abusive_words:
+        if word in content:
+            valid = False
+    if valid:
+        return value
+    else:
+        raise ValidationError("Don't use abusive words")
